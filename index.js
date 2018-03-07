d5c14187c81713590f585aa48a5c6983bf37b65c
Aaron Johnson <aaron.johnson1031@gmail.com>
2018-03-07 12:25:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,73 +11,18 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- var itemName = item,
- rand = Math.floor(Math.random() * 100) + 1;
- var obj = { [itemName] : rand };
- if(cart.push(obj)) {
-   console.log(`${itemName} has been added to your cart.`);
- }
-
- return cart;
+ var rand = Math.floor(Math.random() * 100) + 1,
+     item = item,
+     obj = { item : rand };
+ console.log(`${item} has been added to your cart.`)
 }
 
 function viewCart() {
   // write your code here
-
-  var mess,
-      arr = [],
-      i,
-      newArr,
-      key,
-      value;
-
-  if(cart.length <= 0) {
-    mess = 'Your shopping cart is empty.';
-  } else
-  if(cart.length === 1) {
-    for(i = 0; i < cart.length; i++) {
-      key = Object.keys(cart[i]),
-      value = Object.values(cart[i]);
-    }
-    mess = `In your cart, you have ${key} at $${value}.`;
-  } else
-  if (cart.length === 2) {
-    for(i = 0; i < cart.length; i++) {
-      key = Object.keys(cart[i]),
-      value = Object.values(cart[i]);
-      arr.push(`${key} at $${value}`);
-    }
-    newArr = arr.join(' and ');
-    mess = `In your cart, you have ${newArr}.`;
-  } else
-  if (cart.length > 2) {
-    for(i = 0; i < cart.length; i++) {
-      key = Object.keys(cart[i]),
-      value = Object.values(cart[i]);
-
-      if(i < cart.length - 1) {
-        arr.push(`${key} at $${value}`);
-      } else {
-        arr.push(`and ${key} at $${value}`);
-      }
-    }
-    newArr = arr.join(', ');
-    mess = `In your cart, you have ${newArr}.`;
-  }
-  console.log(mess);
 }
 
 function total() {
-      var totalCost = 0,
-      value,
-      i;
-
-  for(i = 0; i < cart.length; i++) {
-    value = parseInt(Object.values(cart[i]));
-    totalCost += value;
-  }
-
-  return(totalCost);
+  // write your code here
 }
 
 function removeFromCart(item) {
