4b570a2993ae9ba01a9e3d4fe521415065e2c117
Aaron Johnson <aaron.johnson1031@gmail.com>
2018-03-07 17:33:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function viewCart() {
       let itemName = Object.keys(cart[i]);
       let itemPrice = Object.keys(cart[i]);
 
-      if (i === cart.length) {
+      if (i === cart.length - 1) {
         arr.push(`and ${itemName} at $${itemPrice}`);
       } else {
         arr.push(`${itemName} at $${itemPrice}`);
