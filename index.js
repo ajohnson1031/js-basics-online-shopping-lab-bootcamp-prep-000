424928917fe05b84c18a1cff8b9e35e6f9fa14eb
Aaron Johnson <aaron.johnson1031@gmail.com>
2018-03-07 12:22:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,14 +11,73 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemName = item,
+ rand = Math.floor(Math.random() * 100) + 1;
+ var obj = { [itemName] : rand };
+ if(cart.push(obj)) {
+   console.log(`${itemName} has been added to your cart.`);
+ }
+
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+
+  var mess,
+      arr = [],
+      i,
+      newArr,
+      key,
+      value;
+
+  if(cart.length <= 0) {
+    mess = 'Your shopping cart is empty.';
+  } else
+  if(cart.length === 1) {
+    for(i = 0; i < cart.length; i++) {
+      key = Object.keys(cart[i]),
+      value = Object.values(cart[i]);
+    }
+    mess = `In your cart, you have ${key} at $${value}.`;
+  } else
+  if (cart.length === 2) {
+    for(i = 0; i < cart.length; i++) {
+      key = Object.keys(cart[i]),
+      value = Object.values(cart[i]);
+      arr.push(`${key} at $${value}`);
+    }
+    newArr = arr.join(' and ');
+    mess = `In your cart, you have ${newArr}.`;
+  } else
+  if (cart.length > 2) {
+    for(i = 0; i < cart.length; i++) {
+      key = Object.keys(cart[i]),
+      value = Object.values(cart[i]);
+
+      if(i < cart.length - 1) {
+        arr.push(`${key} at $${value}`);
+      } else {
+        arr.push(`and ${key} at $${value}`);
+      }
+    }
+    newArr = arr.join(', ');
+    mess = `In your cart, you have ${newArr}.`;
+  }
+  console.log(mess);
 }
 
 function total() {
-  // write your code here
+      var totalCost = 0,
+      value,
+      i;
+
+  for(i = 0; i < cart.length; i++) {
+    value = parseInt(Object.values(cart[i]));
+    totalCost += value;
+  }
+
+  return(totalCost);
 }
 
 function removeFromCart(item) {
