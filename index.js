81f33e1b9060346fe8b9e5d40896eff922b1e47d
Aaron Johnson <aaron.johnson1031@gmail.com>
2018-03-07 15:54:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,14 +23,20 @@ function addToCart(item) {
 function viewCart() {
   // write your code here
   var i;
-  
+
   if (cart.length <= 0) {
     console.log('Your shopping cart is empty.');
   }
 
   if (cart.length === 1) {
     for (var key in cart) {
-      console.log(`In your cart, you have ${key} at $${cart[key]}`);
+      console.log(`In your cart, you have ${key} at $${cart[key]}.`);
+    }
+
+  }
+
+  console.log(cart);
+}
 
 function total() {
   // write your code here
