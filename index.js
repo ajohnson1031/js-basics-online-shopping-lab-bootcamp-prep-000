94a8920a78413c4a4cf056f3b547543ace93edcb
Aaron Johnson <aaron.johnson1031@gmail.com>
2018-03-07 12:48:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ function viewCart() {
   // write your code here
   var names = Object.keys(cart),
       prices = Object.values(cart);
-      
+
   if (cart.length <= 0) {
     console.log('Your shopping cart is empty.');
   } else if (cart.length === 1) {
-    console.log(`In your cart, you have `)
+    console.log(`In your cart, you have ${names[0]} at $${prices[0]}.`);
   }
 }
 
